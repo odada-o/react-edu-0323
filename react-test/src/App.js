28a196b67cf4073bdeee7d0b097875d1bc9be657
oddoddo <eehd80yul@gmail.com>
2024-04-14 00:57:01 +0900

    ㅇㅇ

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -2,6 +2,7 @@ import Cat from './Cat'
 import Footer from './components/layout/Footer'
 import Header from './components/layout/Header'
 import Home from './components/views/home/Home'
+import News from './components/views/news/New'
 import OrderCoffee, { DrinkCoffee } from './grammar/Asyn'
 import Event from './grammar/Event'
 
@@ -12,7 +13,7 @@ function App() {
 
     return (
         <div className="wrap">
-            <OrderCoffee coffee="아메리카노" time={3000} />
+            <News />
         </div>
     )
 }
